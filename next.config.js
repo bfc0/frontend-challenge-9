9893ec7359da1f15db1ef151f2fb27b4488eebdd
bfc0 <bfcheg@gmail.com>
2023-12-09 02:14:09 +0300

    deploy

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
 const isProd = process.env.NODE_ENV == 'production'
 
 const nextConfig = {
+    assetPrefix: "/frontend-challenge-9",
     basePath: "/frontend-challenge-9",
     output: "export",
     images: {
