02035324b3887d8510f8719a1662a302b047754d
bfc0 <bfcheg@gmail.com>
2023-12-09 02:03:00 +0300

    deploy

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const isProd = process.env.NODE_ENV == 'production'
 
 const nextConfig = {
-    basePath: isProd ? "/frontend-challenge-9" : "",
+    basePath: "/frontend-challenge-9",
     output: "export",
     images: {
         unoptimized: true,
